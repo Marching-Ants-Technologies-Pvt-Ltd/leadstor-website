26caa88f9dec64b87c97dfa263e6a9b1c675c72d
Tanish Raj <help.cttricks@gmail.com>
2024-08-07 10:36:08 +0530

    SEO Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,19 @@ import { Providers } from './providers'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lead Management Software | Lead Tracking | Leadstor",
-  description: "Leadstor is a lead management and tracking system that optimizes your sales process from lead generation to closing. Track and optimize campaigns in real-time, automate follow-ups, and gain actionable insights with detailed analytics. Enhance your sales pipeline and improve conversion rates with Leadstore's intuitive platform.",
-  keywords: "lead management software, lead tracking software, lead generation software, lead software, sales lead tracking software, lead distribution software, customer relationship software,CRM software, CRM System.",
-  robots: "noindex, nofollow",
+  title: "Leadstor | Advanced Lead Management & Tracking Software",
+  description: "Leadstor is a comprehensive lead management and tracking system designed to streamline your sales process from lead generation to closing. Optimize campaigns in real-time, automate follow-ups, and gain actionable insights with detailed analytics. Enhance your sales pipeline and improve conversion rates with Leadstor's intuitive platform.",
+  keywords: "lead management software, lead tracking software, lead generation software, lead software, sales lead tracking software, lead distribution software, customer relationship software, CRM software, CRM System, sales optimization, lead conversion, sales analytics",
+  robots: "index, follow",
+  openGraph: {
+    title: "Leadstor | Advanced Lead Management & Tracking Software",
+    description: "Leadstor is a comprehensive lead management and tracking system designed to streamline your sales process from lead generation to closing. Optimize campaigns in real-time, automate follow-ups, and gain actionable insights with detailed analytics. Enhance your sales pipeline and improve conversion rates with Leadstor's intuitive platform.",
+    type: "website",
+    url: "https://leadstor.in",
+    images: ["https://leadstor.in/banners/leadstor-og-banner.jpg"],
+    siteName: "Leadstor",
+    locale: "en_US"
+  }
 };
 
 export default function RootLayout({
@@ -19,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html  lang="en" suppressHydrationWarning className="scroll-smooth">
+    <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <body>
         <Providers>
           {children}
