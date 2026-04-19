11905dd5edf1ea1e21f50c74b4bec3e20f0bb6ad
ashukla4u <94194668+ashukla4u@users.noreply.github.com>
2026-04-19 21:35:02 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import ProductInfo from '@/components/ProductInfo';
 
 export default function Home() {
   return (
-    <>
+   
       <Navbar />
 
       <header className="border-gray-200 bg-gray-50">
@@ -16,7 +16,7 @@ export default function Home() {
           <div className="grid grid-cols-1 gap-10 py-10 lg:py-24 px-5 sm:px-10 xl:gap-20 xl:px-0 lg:grid-cols-2">
             <div className="m-auto">
               <h1 className="text-2xl sm:text-4xl hero-text-color font-bold text-gray-800 ">Close 2X More Leads Without Hiring More Salespeople with LeadStor</h1>
-              <p className="mt-4 text-base sm:text-lg text-gray-600">Capture, auto assign, and never miss a follow up — all in one AI-powered system</p>
+              <p className="mt-4 text-base sm:text-lg text-gray-600">Capture, auto assign and never miss a follow up — all in one AI-powered system</p>
               <Button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded text-sm px-5 py-2 text-center me-2 mt-8 uppercase" href='/signup '>Create A Free Account</Button>
               <p className="mt-4 text-gray-500 text-sm">Automation is the only way to do more sales than your competition</p>
             </div>
